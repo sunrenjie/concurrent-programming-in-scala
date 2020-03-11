bf0fccea89a9b4824fb320b236fd70e25cc99358
Renjie Sun <sunrenjie6@gmail.com>
2020-03-11 08:06:49 +0800

    ch2 answers: general enhancements and fixes
    Changes:
    1. To be explicit about some val and return value types.
    2. Answer 2.7 is fixed, based on sorting Accounts and locking recursively.
    3. Answers 2.8 and 2.9&10 are fixed; they previously just didn't work.
    4. One App per execution unit.
    5. Other trivial fixes.

--- src/main/scala/org/learningconcurrency/answers/ch2/ch2.scala
+++ src/main/scala/org/learningconcurrency/answers/ch2/ch2.scala
@@ -2,16 +2,14 @@ package org.learningconcurrency
 package answers
 package ch2
 
-object ch2 extends App {
-  //common
-  def thread(body: => Unit): Thread = {
-    val t = new Thread {
-      override def run() = body
-    }
-    t.start()
-    t
-  }
 
+import java.lang.UnsupportedOperationException
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+import org.learningconcurrency.ch2.thread
+
+object ch2ex1 extends App {
 
   // 2.1
   def parallel[A, B](a: () => A, b: () => B): (A, B) = {
@@ -22,13 +20,13 @@ object ch2 extends App {
     var ra = None: Option[A]
     var rb = None: Option[B]
     val ta = new Thread {
-      override def run() = {
+      override def run(): Unit = {
         ra = Some(a())
       }
     }
     ta.start()
     val tb = new Thread {
-      override def run() = {
+      override def run(): Unit = {
         rb = Some(b())
       }
     }
@@ -46,14 +44,18 @@ object ch2 extends App {
   }
   parallel(fa, fb)
 
+}
 
+
+object ch2ex2 extends App {
   // 2.2
   // This implementation is inefficient: setting the thread as daemon and
   // placing the endless loop within is much better
   def periodically(duration: Long, b: => Unit): Unit = {
+    // Here for each invocation, a new Thread is created. The textbook code use a single thread instead.
     while (true) {
       val t = new Thread {
-        override def run() = {
+        override def run(): Unit = {
           b
         }
       }
@@ -66,6 +68,10 @@ object ch2 extends App {
   periodically(1000, {
     println("Hello")
   })
+}
+
+
+object ch2ex3 extends App {
 
   // 2.3
   // Here the choice of target of synchronized is important. The variable 't' at
@@ -122,7 +128,7 @@ object ch2 extends App {
   val x = new SyncVar[Int]()
   x.put(1)
   val t = new Thread {
-    override def run() = {
+    override def run(): Unit = {
       Thread.sleep(5000)
       val v = x.getWait
       println("thread getWait() value " + v.toString)
@@ -130,14 +136,17 @@ object ch2 extends App {
   }
   t.start()
   x.putWait(3)
+}
 
+
+object ch2ex6 extends App {
 
   // 2.6 SyncVar class that hold at most n values
   class SyncQueue[T](capacity: Int) {
 
     import scala.collection.mutable
 
-    val data = mutable.Queue[T]()
+    private val data = mutable.Queue[T]()
 
     def get(): T = this.synchronized {
       if (data.isEmpty)
@@ -162,7 +171,7 @@ object ch2 extends App {
       data.nonEmpty
     }
 
-    def getWait(): T = this.synchronized {
+    def getWait: T = this.synchronized {
       while (data.isEmpty) this.wait()
       val x = data.dequeue()
       this.notify()
@@ -178,28 +187,33 @@ object ch2 extends App {
 
   val q = new SyncQueue[Int](3)
   val th = new Thread {
-    override def run() = {
-      var x = q.getWait()
-      println("thread: get($x) is done")
-      x = q.getWait()
-      println("thread: get($x) is done")
+    override def run(): Unit = {
+      var x = q.getWait
+      println("thread: get($x) is done with val=" + x)
+      x = q.getWait
+      println("thread: get($x) is done with val=" + x)
     }
   }
   th.start()
   Thread.sleep(5000)
 
-  q.put(1)
-  q.put(2)
-  q.put(3)
-  q.put(4)
+  for {x <- 1 until 4} {
+    println("Main: will call put() with val=" + x)
+    q.put(x)
+    println("Main: will sleep for 1s")
+    Thread.sleep(1000)
+  }
   println("thread of 4 put() is done")
+}
 
+object ch2ex7 extends App {
 
   // 2.7
-  // TODO incorrect solution
-  /*
+  // This solution assigns an unique and immutable ID for each Account during creation. Then in sendAll(),
+  // the accounts are sort and recursively locked. The immutable order of accounts are locked prevents dead lock.
+  // A non-recursive solution shall be based on some explicit JDK lock.
 
-  The sendAll() is atomic; we've got to lock all accounts in the first place.
+  /*
   The reference answer make use of a recursive locking technique:
   def recur(li: List[Account]): Unit = li match {
     case h :: rest => h.synchronized {
@@ -211,18 +225,20 @@ object ch2 extends App {
   recur(Account("foo", 1)::(Account("bar", 2)::Nil)
 
    */
+
+  // To generate unique IDs in a synchronized manner.
   object SynchronizedProtectedUid extends App {
 
     var uidCount = 0
 
-    def getUniqueId() = this.synchronized {
+    def getUniqueId = this.synchronized {
       val freshUid = uidCount + 1
       uidCount = freshUid
       freshUid
     }
 
     def printUniqueIds(n: Int): Unit = {
-      val uids = for (i <- 0 until n) yield getUniqueId()
+      val uids = for (i <- 0 until n) yield getUniqueId
       println(s"Generated uids: $uids")
     }
 
@@ -234,55 +250,66 @@ object ch2 extends App {
 
   }
 
-
-  object SynchronizedDeadlock extends App {
-
-    import SynchronizedProtectedUid._
-
-    class Account(val name: String, var money: Int) {
-      val uid = getUniqueId()
-    }
-
-    def sendAll(accounts: Set[Account], target: Account): Unit = target.synchronized {
-      accounts foreach { a => assert(a.uid != target.uid) }
-      accounts foreach {
-        a => {
-          a.synchronized {
-            target.money += a.money
-            a.money = 0
-          }
-        }
-      }
-    }
-
-    val a = new Account("Jill", 1000)
-    val b = new Account("Jack", 2000)
-    val c = new Account("Ellis", 3000)
-    val d = new Account("Bob", 3000)
-    val e = new Account("Neil", 3000)
-    val f = new Account("Tom", 3000)
-    val set = Set(a, b, c, d, e)
-    sendAll(set, f)
-    println(s"f = ${f.money}")
+  case class Account(val name: String, var money: Int) {
+    val uid: Int = SynchronizedProtectedUid.getUniqueId
   }
 
+  def syncRecursivelyIterAndExecute(accounts: List[Account], i: Int, job: () => Unit): Unit = {
+    if (i == accounts.length) {
+      job()
+      return
+    }
+    accounts(i).synchronized {
+      syncRecursivelyIterAndExecute(accounts, i + 1, job)
+    }
+  }
+
+  def syncRecursivelyAndExecute(accounts: Set[Account], job: () => Unit): Unit = {
+    val li = accounts.toList
+    li.sortBy(a => a.uid)
+    syncRecursivelyIterAndExecute(li, 0, job)
+  }
+
+  def sendAll(accounts: Set[Account], target: Account): Unit = target.synchronized {
+    accounts foreach { a => assert(a.uid != target.uid) }
+    syncRecursivelyAndExecute(accounts, () => {
+      accounts foreach {
+        a => {
+          target.money += a.money
+          a.money = 0
+        }
+      }
+    })
+  }
+
+  val a = Account("Jill", 1000)
+  val b = Account("Jack", 2000)
+  val c = Account("Ellis", 3000)
+  val d = Account("Bob", 3000)
+  val e = Account("Neil", 3000)
+  val f = Account("Tom", 3000)
+  val set = Set(a, b, c, d, e)
+  sendAll(set, f)
+  println(s"f = ${f.money}")
+}
+
+object ch2ex8 extends App {
 
   // 2.8
-  import scala.annotation.tailrec
-  import scala.collection.mutable
-
-  object PriorityTaskPool extends App {
+  object PriorityTaskPool {
 
     // PriorityQueue code stolen from text book code and from:
     // http://stackoverflow.com/questions/789250/scala-is-there-a-way-to-use-priorityqueue-like-i-would-in-java
-
-    class Job(task: => Unit, pri: Int) {
-      val job: () => Unit = () => task
-      val priority = pri
+    // Policy: the worker thread will be created as user thread (isDaemon=false). Upon shutdown(), it will not
+    // accept new submissions and fail with UnsupportedOperationException if there are any. After all tasks are
+    // finished, it will exit.
+    case class Job(task: () => Unit, pri: Int) {
+      val job: () => Unit = task
+      val priority: Int = pri
     }
 
     implicit def orderedJob(j: Job): Ordered[Job] = new Ordered[Job] {
-      def compare(other: Job) = j.priority.compare(other.priority)
+      def compare(other: Job): Int = j.priority.compare(other.priority)
     }
 
     val tasks = new mutable.PriorityQueue[Job]()
@@ -292,35 +319,55 @@ object ch2 extends App {
 
       def poll(): Option[Job] = tasks.synchronized {
         while (tasks.isEmpty && !terminated) tasks.wait()
-        if (!terminated) Some(tasks.dequeue()) else None
+        if (tasks.isEmpty)
+          None
+        else
+          Some(tasks.dequeue())
       }
 
-      @tailrec override def run() = {
+      @tailrec override def run(): Unit = {
         // being extremely lazy to make the asynchronous nature obvious
-        Thread.sleep(5000)
+        println("Worker thread: sleeping for a while to emulate being overloaded ...")
+        Thread.sleep(300)
         poll() match {
           case Some(task) => task.job(); run()
-          case None =>
+          case None => if (!terminated) run()
         }
       }
 
-      def shutdown() = tasks.synchronized {
-        terminated = true
-        tasks.notify()
-      }
+      this.setDaemon(false)
     }
 
-    def asynchronous(priority: Int)(task: => Unit): Unit = tasks.synchronized {
-      tasks.enqueue(new Job(task, priority))
+    def shutdown(): Unit = tasks.synchronized {
+      Worker.terminated = true
+      // To notify the thread(s) sleeping waiting for new tasks to wake up for this new event.
+      tasks.notifyAll()
+    }
+
+    def asynchronous(priority: Int)(task: () => Unit): Unit = tasks.synchronized {
+      if (Worker.terminated)
+        throw new UnsupportedOperationException("terminated pool does not accept new job")
+      tasks.enqueue(Job(task, priority))
       tasks.notify()
     }
 
     Worker.start()
-    Range(0, 9).reverse foreach { i => asynchronous(i)(println(s"println $i")) }
   }
 
-  PriorityTaskPool.main(Array[String]())
+  val p = PriorityTaskPool
+  Range(0, 9).reverse foreach { i => p.asynchronous(i)(() => println(s"Async job: println $i")) }
+  p.shutdown()
+  try {
+    Range(0, 1).reverse foreach { i => p.asynchronous(i)(() => println(s"Async job after shutdown(): println $i")) }
+  } catch {
+    case _: UnsupportedOperationException =>
+      println("An UnsupportedOperationException is thrown upon submitting new task after shutdown(), as expected.")
+  }
+}
 
+object ch2ex9and10 extends App {
+  // A multi-thread pool that executes jobs in the order based on descending importance values.
+  // Upon shutdown(), no new jobs will be accepted; existing jobs
 
   // TODO this solution is correct, yet over-complicated
   // Actually, we could go without the unimportant flag, along with the complex
@@ -333,13 +380,13 @@ object ch2 extends App {
   // 2.9 and 2.10
   class PriorityTaskPoolWithMultipleThreads(numThreads: Int, importance: Int) {
 
-    class Job(task: => Unit, pri: Int) {
-      val job: () => Unit = () => task
-      val priority = pri
+    case class Job(task: () => Unit, pri: Int) {
+      val job: () => Unit = task
+      val priority: Int = pri
     }
 
     implicit def orderedJob(j: Job): Ordered[Job] = new Ordered[Job] {
-      def compare(other: Job) = j.priority.compare(other.priority)
+      def compare(other: Job): Int = j.priority.compare(other.priority)
     }
 
     val tasks = new mutable.PriorityQueue[Job]()
@@ -363,7 +410,7 @@ object ch2 extends App {
           //    will find their way here anyway (no new tasks will be accepted
           //    when terminated is true).
           None
-        } else {
+        } else { // terminated and tasks list is not empty and may contain potentially important tasks
           val t = tasks.dequeue()
           if (!terminated || t.priority > importance) {
             Some(t)
@@ -374,27 +421,29 @@ object ch2 extends App {
         }
       }
 
-      @tailrec final override def run() = {
-        Thread.sleep(5000)
+      @tailrec final override def run(): Unit = {
+        Thread.sleep(300)
+        val name = this.toString
+        println(s"Worker thread $name: sleeping for a while to emulate being overloaded ...")
         poll() match {
-          case Some(task) =>
-            if (task.priority > importance)
-              task.job()
-            run()
-          case None =>
+          case Some(task) => task.job(); run()
+          case None => if (!terminated) run()
         }
       }
+
+      this.setDaemon(false)
     }
 
-    def shutdown() = tasks.synchronized {
+    def shutdown(): Unit = tasks.synchronized {
       terminated = true
-      tasks.notify()
+      // To notify all workers up for the new state of being terminated
+      tasks.notifyAll()
     }
 
-    def asynchronous(priority: Int)(task: => Unit): Unit = tasks.synchronized {
+    def asynchronous(priority: Int)(task: () => Unit): Unit = tasks.synchronized {
       if (terminated)
         throw new UnsupportedOperationException("terminated pool does not accept new job")
-      tasks.enqueue(new Job(task, priority))
+      tasks.enqueue(Job(task, priority))
       tasks.notify()
     }
 
@@ -403,9 +452,14 @@ object ch2 extends App {
   }
 
   // The messages will appear roughly in groups of numThreads ones.
-  val p = new PriorityTaskPoolWithMultipleThreads(3, 20)
-  Range(0, 20).reverse foreach { i => p.asynchronous(i)(println(s"println $i")) }
+  val p = new PriorityTaskPoolWithMultipleThreads(2, 10)
+  Range(0, 20).reverse foreach { i => p.asynchronous(i)(() => println(s"Async job: println $i")) }
   p.shutdown()
   // This will raise no-new-job-allowed exception:
-  Range(0, 1).reverse foreach { i => p.asynchronous(i)(println(s"println $i")) }
+  try {
+    Range(0, 1).reverse foreach { i => p.asynchronous(i)(() => println(s"Async job after shutdown(): println $i")) }
+  } catch {
+    case _: UnsupportedOperationException =>
+      println("An UnsupportedOperationException is thrown upon submitting new task after shutdown(), as expected.")
+  }
 }
--- src/main/scala/org/learningconcurrency/ch2/package.scala
+++ src/main/scala/org/learningconcurrency/ch2/package.scala
@@ -1,19 +1,14 @@
 package org.learningconcurrency
 
 
-
-
-
-
 package object ch2 {
 
-  def thread(body: =>Unit): Thread = {
+  def thread(body: => Unit): Thread = {
     val t = new Thread {
-      override def run() = body
+      override def run(): Unit = body
     }
     t.start()
     t
   }
 
 }
-
